537d0f4ee5a18d01724b38e737f3e9ed57d1cc0a
ZeinabHom <91231253+ZeinabHom@users.noreply.github.com>
2021-12-16 01:50:10 -0500

    add Pewlett Hackard project

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,31 @@
+
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       ti.title,
+       ti.from_date,
+       ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) 
+	   		rt.emp_no,
+      		rt.first_name,
+       		rt.last_name,
+      		 rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+
+ORDER BY emp_no ASC , to_date DESC;
+
+
+
+
+
+
+
